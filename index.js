11824b24e601a9c77669e6bb83f9a14504dee29a
Zach Bjornson <zbbjornson@gmail.com>
2023-11-24 17:00:43 -0800

    bug: wrong detection because of operator precedence

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ const ECX = 2;
 const EDX = 3;
 
 function supportsAVX() {
-	return cpuid(ECX, 1) & (1 << 28) !== 0;
+	return (cpuid(ECX, 1) & (1 << 28)) !== 0;
 }
 
 function supportsAVX2() {
-	return cpuid(EBX, 7) & (1 << 5) !== 0;
+	return (cpuid(EBX, 7) & (1 << 5)) !== 0;
 }
 
 function supportsAVX512() {
 	// 30 is AVX512_BW.
-	return cpuid(EBX, 7) & (1 << 30) !== 0;
+	return (cpuid(EBX, 7) & (1 << 30)) !== 0;
 }
 
 module.exports.getArch = function getArch() {
